e73bd201a831f81163e6d994440654eec426def5
Chris Feijoo <chris@kube.io>
2025-06-02 11:53:13 +0200

    Configure MDX with Mermaid and PrettyCode

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import mdx from "@mdx-js/rollup";
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
+import rehypeMermaid from "rehype-mermaid";
+import rehypePrettyCode from "rehype-pretty-code";
 import remarkFrontmatter from "remark-frontmatter";
 import remarkMdxFrontmatter from "remark-mdx-frontmatter";
 import { defineConfig } from "vite";
@@ -12,7 +14,13 @@ import faviconsPlugin from "./faviconsPlugin";
 export default defineConfig({
   plugins: [
     tailwindcss(),
-    mdx({ remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter] }),
+    mdx({
+      remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter],
+      rehypePlugins: [
+        rehypeMermaid,
+        [rehypePrettyCode, { theme: "one-dark-pro" }],
+      ],
+    }),
     vanillaExtractPlugin(),
     reactRouter(),
     faviconsPlugin(createFavicon(), {
