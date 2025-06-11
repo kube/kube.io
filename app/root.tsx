7bc14954010b3b422eb20567133d9d28bb53539e
Chris Feijoo <chris@kube.io>
2025-06-12 00:16:59 +0200

    Add DM Mono font

--- app/root.tsx
+++ app/root.tsx
@@ -43,6 +43,16 @@ export function Layout({ children }: { children: React.ReactNode }) {
         />
         <link rel="preconnect" href="https://rsms.me/" />
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=DM+Mono:ital,wght@0,300;0,400;0,500;1,300;1,400;1,500&display=swap"
+          rel="stylesheet"
+        />
 
         <script
           defer
